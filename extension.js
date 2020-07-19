618b578855b475d48316e5f6aaa04cacb94be493
HarshaVardhanNakkina <harshavardhan.n@yahoo.com>
2020-07-19 18:43:47 +0530

    changed how algorithms are chosen

--- extension.js
+++ extension.js
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode')
 
 // UTILS
@@ -13,21 +11,6 @@ const showLanguagePicker = require('./utils/showLanguagePicker')
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	// console.log('Congratulations, your extension "algosnips" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	// let disposable = vscode.commands.registerCommand('algosnips.helloWorld', function () {
-	// The code you place here will be executed every time your command is executed
-
-	// Display a message box to the user
-	// vscode.window.showInformationMessage('Hi this is algosnips!');
-	// });
-
-	// context.subscriptions.push(disposable);
 	vscode.commands.registerCommand('algosnips.search', async () => {
 		// Show search bar to search for a snippet
 		let searchStr = await showSearchInput().catch((err) => {
